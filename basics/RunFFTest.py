e5de3c9265901525181116e2de191b6c5d7bda2c
mageshwarant <48029648+mageshwarant@users.noreply.github.com>
2019-07-16 22:50:28 +0530

    Geckodriver change

--- basics/RunFFTest.py
+++ basics/RunFFTest.py
@@ -6,8 +6,7 @@ class RunFFTest():
 
     def test(self):
 
-        # driverLocation = "C:\\Users\\hp\\Documents\\GitHub\\Coding\\Python\\chromedriver.exe"
-        # os.environ["webdriver.chrome.driver"] = driverLocation
+
 
         driver = webdriver.Firefox()
 
