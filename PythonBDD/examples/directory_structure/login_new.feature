6ff00bf71e8f9432df430321612f838a7767c256
mageshwarant <48029648+mageshwarant@users.noreply.github.com>
2019-08-16 01:22:21 +0530

    1st BDD framework created

---
+++ PythonBDD/examples/directory_structure/login_new.feature
@@ -0,0 +1,8 @@
+
+Feature: Dummy Set for Print
+
+Scenario: Test_case_1
+
+    Given I am in a directory
+    Then I am also in a directory
+
---
+++ PythonBDD/examples/directory_structure/steps/steps.py
@@ -0,0 +1,9 @@
+form Behave import given, when, then
+
+@given("I am in a directory")
+def in_main_dir(context):
+    print("In Main directory")
+
+@then("I am also in a directory")
+def in_add_dir(context):
+    print("Also in Main dir")
